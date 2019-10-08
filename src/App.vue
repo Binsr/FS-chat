2b7c9a6fa77adac3de9ec90fb13814048bea100b
Binsr <urosjovanovic.sna@gmail.com>
2019-10-09 01:48:36 +0200

    a

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,131 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <startPage>
+      
+    </startPage>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import startPage from './components/startPage.vue'
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    startPage
   }
 }
 </script>
 
 <style>
+
+.Message{
+  width: auto;
+  height: auto;
+  min-height: 20px;
+  background-color: rgba(51, 51, 51, 0.6);
+  float: right;
+  margin: 7px;
+  padding-right: 40px;
+  font-size: 14px;
+  border-style: solid;
+  border-radius: 15px;
+  border-width: 1px;
+  padding-top: 3px;
+  color: rgb(255, 255, 255);
+  max-width: 400px;
+  /* margin-left: 400px; */
+  padding-left: 10px;
+  font-weight: 300;
+  word-break: break-all;
+  display: flex;
+  flex-direction: column;
+    
+}
+.messageText{
+  float: left;
+  text-align: left;
+  /* word-wrap:break-word; */
+}
+/* .wrapLine{
+  position:relative;
+  display:block;
+  width: 400px;
+  height: fit-content;
+  padding-left: 300px;
+  float: right;
+} */
+
+.wrapLine{
+  display:flex;
+  max-width: 500px;
+  height: fit-content;
+  flex-wrap:  wrap;
+  justify-content:flex-end;
+  margin-right: 0;
+}
+
+.time{
+  font-size: 10px;
+  margin-left: 5px;
+}
+.timeWrap{
+  color: rgb(160, 159, 159);
+  background-color: rgba(255, 255, 255, 0);
+  display:flex;
+  justify-content: flex-end;
+  margin-right: -25px;
+  margin-bottom: 3px;   
+}
+.chatWrap{
+  align-content: center;
+  display: inline-block; 
+  color: white;
+}
+.ChatWindow{
+  position: relative;
+  width: 550px;
+  height: 550px;
+  background-color: rgb(0, 0, 0);
+  border-width: 3px;
+  border-color: green;
+  border-style:solid;
+  overflow-y: scroll;
+  display: flexbox;
+  /* flex-direction: column; */
+}
+
+.TypeBarTextArea{
+  margin-top: 10px;
+  margin-left: 50px;
+  width: 400px;
+  height: 30px;
+  background-color: rgb(51, 51, 51);
+  font-size: 18px;
+  color: rgb(255, 255, 255);
+  position: relative;
+  padding-left: 10px;
+  border-radius: 15px;
+}
+
+.imgButton{
+  position: relative;
+  float: right;
+  right: 43px;
+  bottom: 33px;
+  border-radius: 30%;
+  width:32px;
+  height: 30px;
+  border-radius: 25%;
+  background-color: green;
+  border-style: solid;
+  border-width: 2px;
+  border-color: green;
+  /* display: none; */
+}
+.inputHide{
+  display: none;
+}
 #app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
---
+++ src/components/startPage.vue
@@ -0,0 +1,206 @@
+<template>
+<div>
+    <div class="tatkoNaMafiu">
+        <div class="headChat">
+                <div class="topHead">
+                    <div class="chatIcon"></div>
+                    <div class="roomWrap"><p class="roomName">F-S area: {{client.name}}</p></div>
+                </div>
+                <p class="botHead">Number of users online:{{client.users}}</p>
+        </div>
+        <div class="chatWrap">
+            <div class="ChatWindow"> 
+                <div>
+                    <div class="singleMessageDiv"
+                    v-for="(msg, index) in message"
+                    v-bind:key="index"
+                    >
+                        <div class="wrapLine">
+                            <div class="Message">
+                                <div class="messageText"> {{msg.content}} </div>
+                                <div class="timeWrap">
+                                    <div class="time"> {{msg.time}} </div>    
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="inputWrap">
+                <input type="text" v-model="myMessage">
+                <button @click="submit">asdas</button>
+                <br>
+                <span>{{myMessage}}</span>
+            </div>
+        
+            <!-- <form onSubmit={this.handleSubmit} >
+                <input  type="text" class= "TypeBarTextArea" onChange={event => this.handleChange(event)} >
+                </input>
+            </form>
+                    <input type="file" class="inputHide" id="fileupload" 
+                    onChange={this.imgHandle} 
+                    ref={fileInput => this.fileInput = fileInput}/>
+                    <button class="imgButton" @click="nekafunckija" ></button> -->
+        </div>
+    </div>
+</div>
+</template>
+
+<script>
+export default {
+    data(){
+        return{
+            client:{
+                "name":"djordje",
+                "users": 13
+            },
+            myMessage:'',
+            message:[
+                {
+                "content":"asda as das asd as da s ASfasfafasfas",
+                "time":'23:14'
+                },{
+                "content":"asjdsajasdaasdasdasasddsadasdaasdasdasdadsadadsdsadasasdasdasdasdasdasdasdasd",
+                "time":'23:14'
+                },{
+                "content":"asjdsajasdaasdasdasasddsadasdaasdasdasdadsadadsdsadasasdasdasdasdasdasdasdasd",
+                "time":'23:14'
+                },{
+                "content":"asjdsajasdaasdasdasasddsadasdaasdasdasdadsadadsdsadasasdasdasdasdasdasdasdasd",
+                "time":'23:14'
+                },{
+                "content":"asjdsajasdaasdasdasasddsadasdaasdasdasdadsadadsdsadasasdasdasdasdasdasdasdasd",
+                "time":'23:14'
+                },{
+                "content":"asjdsajasdaasdasdasasddsadasdaasdasdasdadsadadsdsadasasdasdasdasdasdasdasdasd",
+                "time":'23:14'
+                },{
+                "content":"asjdsajasdaasdasdasasddsadasdaasdasdasdadsadadsdsadasasdasdasdasdasdasdasdasd",
+                "time":'23:14'
+                },{
+                "content":"asjdsajasdaasdasdasasddsadasdaasdasdasdadsadadsdsadasasdasdasdasdasdasdasdasd",
+                "time":'23:14'
+                },{
+                "content":"asjdsajasdaasdasdasasddsadasdaasdasdasdadsadadsdsadasasdasdasdasdasdasdasdasd",
+                "time":'23:14'
+                },{
+                "content":"asjdsajasdaasdasdasasddsadasdaasdasdasdadsadadsdsadasasdasdasdasdasdasdasdasd",
+                "time":'23:14'
+                },{
+                "content":"asjdsajasdaasdasdasasddsadasdaasdasdasdadsadadsdsadasasdasdasdasdasdasdasdasd",
+                "time":'23:14'
+                },{
+                "content":"asjdsajasdaasdasdasasddsadasdaasdasdasdadsadadsdsadasasdasdasdasdasdasdasdasd",
+                "time":'23:14'
+                },{
+                "content":"asjdsajasdaasdasdasasddsadasdaasdasdasdadsadadsdsadasasdasdasdasdasdasdasdasd",
+                "time":'23:14'
+                },{
+                "content":"asjdsajasdaasdasdasasddsadasdaasdasdasdadsadadsdsadasasdasdasdasdasdasdasdasd",
+                "time":'23:14'
+                }
+            ]
+            
+        }
+    },
+    name: 'startPage',
+    props: {
+        msg: String
+    },
+    methods:{
+        hello() {
+            console.log('hello');
+            
+        },
+        submit() {
+            // axios.post('ahsdhas').data(this.myMessage)
+            let timeHours = new Date().getHours().toString;
+            let timeMin = new Date().getMinutes().toString;
+            if(timeHours < 10 ){
+                timeHours = '0'+timeHours
+            }
+            if(timeMin < 10 ){
+                timeMin = '0'+timeMin
+            }
+            let timeNow = timeHours + ":" + timeMin;
+            let currentMessage = this.myMessage;
+            let newMsg = {
+                "content":currentMessage,
+                "time":'12:32'
+            }
+            this.message.push(newMsg);
+            this.myMessage = '';
+        }
+    },
+    created () {
+        //ovde idu api pozivi koji ucitavaju sadrzaj
+    },
+    mounted () {
+        this.hello();
+    }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style >
+.singleMessageDiv{
+    display: flex;
+    justify-content: flex-end;
+}
+.tatkoNaMafiu{
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+}
+.chatIcon{
+    width: 60px;
+    height: 60px;
+    border-radius: 50%;
+    background-color: white;
+    align-self: flex-start;
+    margin: 9px 0 0 9px;
+    
+}
+.headChat{
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
+    width: 550px;
+    background-color: rgb(22, 88, 20);
+}
+.topHead{
+    display: flex;
+}
+.botHead{
+    color: rgb(133, 133, 133);
+    font-size: 10px;
+}
+.roomName{
+    font-size: 20px;
+    color: rgb(33, 160, 29);
+    font-size: 20px;
+    color: rgb(33, 160, 29);
+    font-family: "Righteous", cursive;
+    margin-left: -60px;
+}
+.roomWrap{
+    width: 100%;
+display: flex;
+align-items: center;
+justify-content: center;
+}
+.inputWrap{
+    background-color: rgb(20, 82, 20);
+}
+/*
+CHATHEAD
+    <div class="topHead">
+        <div class="chatIcon"></div>
+        <p class="roomName">Chat area:</p>
+    </div>
+    <p class="botHead">Number of users online:</p>
+*/
+
+
+</style>
