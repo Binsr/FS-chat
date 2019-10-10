d56c0ae39d51bd49b82248172707f816d30a6030
Binsr <urosjovanovic.sna@gmail.com>
2019-10-10 16:13:01 +0200

    Dodata stranica

---
+++ src/components/chatPage.vue
@@ -0,0 +1,182 @@
+<template>
+<div>
+    <div class="tatkoNaMafiu">
+        <div class="headChat">
+                <div class="topHead">
+                    <div class="chatIcon"></div>
+                    <div class="roomWrap"><p class="roomName">F-S area: {{client.name}}</p></div>
+                </div>
+                <p class="botHead">Number of users online:{{client.users}}</p>
+        </div>
+        <div class="chatWrap">
+            <div class="ChatWindow"> 
+                <div>
+                    <div class="singleMessageDiv"
+                    v-for="(msg, index) in message"
+                    v-bind:key="index"
+                    >
+                        <div class="wrapLine">
+                            <div class="Message">
+                                <div class="messageText"> {{msg.content}} </div>
+                                <div class="timeWrap">
+                                    <div class="time"> {{msg.time}} </div>    
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="inputWrap">
+                <input  type="text" v-model="myMessage">
+                <button @keyup.enter.native="submit" @click="submit">--></button>
+            </div>
+        
+            <!-- <form onSubmit={this.handleSubmit} >
+                <input  type="text" class= "TypeBarTextArea" onChange={event => this.handleChange(event)} >
+                </input>
+            </form>
+                    <input type="file" class="inputHide" id="fileupload" 
+                    onChange={this.imgHandle} 
+                    ref={fileInput => this.fileInput = fileInput}/>
+                    <button class="imgButton" @click="nekafunckija" ></button> -->
+        </div>
+    </div>
+</div>
+</template>
+
+<script>
+export default {
+    data(){
+        return{
+            client:{
+                "name":"Uros",
+                "users": 13
+            },
+            otherMessages: [],
+            myMessage:'',
+            message:[]
+        }
+    },
+    name: 'chatPage',
+    props: {
+        msg: String
+    },
+    methods:{
+        submit() {
+            let time = new Date();
+            let timeNow = time.getHours() + ":" + time.getMinutes();
+            let currentMessage = this.myMessage;
+            let newMsg = {
+                "content":currentMessage,
+                "time": timeNow
+            }
+            this.message.push(newMsg);
+            this.myMessage = '';
+        },
+        // loadpage () {
+        //     this.otherMessages = "picka";
+        //     axios.get("http://990b121.mars1.mars-hosting.com/hello%20:D")
+        //     .then(function(response){
+        //         this.otherMessages.push = response.data.bpi;
+        //     })
+        //     .catch(function(error){
+        //         this.otherMessages = error;
+        //     })
+        // }
+    },
+    created () {
+        this.loadpage();
+    },
+    mounted () {
+        this.hello();
+    }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style >
+.singleMessageDiv{
+    display: flex;
+    justify-content: flex-end;
+}
+.tatkoNaMafiu{
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+}
+.chatIcon{
+    width: 60px;
+    height: 60px;
+    border-radius: 50%;
+    background-color: white;
+    align-self: flex-start;
+    margin: 9px 0 0 9px;
+    background-image: url("../assets/avatar.png");
+    background-size: cover;
+    background-position: center center;
+    background-repeat: no-repeat;
+}
+.headChat{
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
+    width: 550px;
+    background-color: rgb(0, 0, 0);
+}
+.topHead{
+    display: flex;
+}
+.botHead{
+    color: rgb(133, 133, 133);
+    font-size: 10px;
+}
+.roomName{
+    font-size: 20px;
+    color: rgb(33, 160, 29);
+    font-size: 20px;
+    color: rgb(33, 160, 29);
+    font-family: "Righteous", cursive;
+    margin-left: -60px;
+}
+.roomWrap{
+width: 100%;
+display: flex;
+align-items: center;
+justify-content: center;
+}
+.inputWrap{
+    background-color: rgb(20, 82, 20);
+}
+.inputWrap input{
+    width: 88%;
+    display: block;
+    float: left;
+    border-radius: 30px;
+    background-color: gray;
+    font-size: 18px;
+    padding-left: 5px;
+}
+.inputWrap button{
+    color: rgb(20, 233, 13);
+    padding-top: 2px;
+    background-color: rgb(0, 0, 0);
+    font-weight: bolder;
+    font-family: sans-serif;
+    font-size: 18px;
+    width: 9%;
+    border: none;
+    display: block;
+    float: right;
+    border-radius: 30px;
+}
+/*
+CHATHEAD
+    <div class="topHead">
+        <div class="chatIcon"></div>
+        <p class="roomName">Chat area:</p>
+    </div>
+    <p class="botHead">Number of users online:</p>
+*/
+
+</style>
