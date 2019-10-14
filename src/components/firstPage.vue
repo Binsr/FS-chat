2466b092118cb9bccc2977631f8d6c037b74fd4a
Binsr <urosjovanovic.sna@gmail.com>
2019-10-14 15:53:15 +0200

    logo

--- src/components/firstPage.vue
+++ src/components/firstPage.vue
@@ -6,7 +6,7 @@
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <meta http-equiv="X-UA-Compatible" content="ie=edge">
-        <link rel="icon" type="image/ico" href="resources/logo.jpg"/>
+        <link rel="icon" type="image/ico" href="../assets/logo.jpg"/>
         <link href="https://fonts.googleapis.com/css?family=Shojumaru&display=swap" rel="stylesheet">
         <link href="https://fonts.googleapis.com/css?family=Gloria+Hallelujah&display=swap" rel="stylesheet">
         <link href="https://fonts.googleapis.com/css?family=Permanent+Marker&display=swap" rel="stylesheet">
