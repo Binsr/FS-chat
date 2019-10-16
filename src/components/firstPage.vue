8bfad7e1c407699dbfbb0cee3dc64aab7e019b35
Drndarevic Milan <drnica54@gmail.com>
2019-10-16 22:23:36 +0200

    milan

--- src/components/firstPage.vue
+++ src/components/firstPage.vue
@@ -2,20 +2,16 @@
 // KAD SE VIDIMO VIDACE OBJASNI MI POCICIONIRANJE PARETNT CHILD MARGIN: 0 AUTO I TAKO TO
 <template>
   <div class="tatko">
+    <div class="images" v-for="image in images">
+     <img v-bind:src ="image" alt="cafeAdd">
+     </div>
     <div class="wrap">
-      <header>
-        <h1 class="title"><span class="thisIsPart">this is</span>
-          <br><span class="titleFSpart">F - S </span>
-          <span class="areaPart"><br> App</span>
-        </h1>
-      </header>
       <div class="monitor">
         <p class="textType" id="text"></p>
       </div>
       <button class="getInBtn" @click="toChatPage"><p class="btParagraf" id="clickme">get in</p>
-        <div class="imageShip">
-          <img src="../assets/mainlogo.jpg"/>
-        </div>
+      </button>
+      <button class="getInBtn"><p class="btParagraf" id="konobar">call</p>
       </button>
     </div>
   </div>
@@ -30,7 +26,8 @@ export default {
     name: "startPage",
     data() {
       return{
-        interval:null
+        interval:null,
+        images:['/img/1.76538e7c.jpg','/img/2.8988a771.jpg','/img/3.395c94cd.jpg','/img/4.2e42ee66.jpg']
       }
     },
     methods: {
@@ -68,9 +65,11 @@ export default {
             }
             if (j % 20 == 0) {
               document.getElementById("clickme").style.color = "rgb(255, 0, 0)";
+              document.getElementById("konobar").style.color = "rgb(255, 0, 0)";
             }
             if (j % 40 == 0) {
               document.getElementById("clickme").style.color = "rgb(0, 0, 0)";
+              document.getElementById("konobar").style.color = "rgb(0, 0, 0)";
             }
             if (j == 40) {
               j = 0;
@@ -101,108 +100,78 @@ export default {
 </script>
 
 <style>
-.tatko{
-    background-color: #000000;
-    display: flex;
-    align-items: center;
-    justify-content: center;
-    align-content: center;
-    margin: 0 auto;
+.clearfix::after{
+  content: '';
+  clear: both;
+  display: block;
+  height: 1px;
+  visibility: hidden;
 }
-.wrap{
-    width: 100vw;
+#poptext{
+    color: cyan;
+    font-weight: bolder;
+    margin: 2px 2px;
+}
+.popwrap{
     height: 100vh;
+    width: 100vw;
+    background-color: #000;
 }
-
-
-header {
-  background: linear-gradient(
-    #000000,
-    rgb(41, 41, 41),
-    rgb(28, 196, 6),
-    #000000
-  );
-  height: 200px; /* Deo za promenu  */
+ .getInBtn {
+  width: 40%;
+  display: block;
+  float: left;
+  height: 20%;
   font-size: 20px;
-  font-family: "Righteous", cursive, "Ultra", serif;
-  width: 100%;
+  margin: 20px;
+  background-color: rgba(224, 20, 20, 0);
+  border-radius: 100px;
+  border-width: 4px;
+  border-style: solid;
+  border-color: rgb(119, 158, 122);
+  max-height: 200px;
+  max-width: 300px;
+  cursor: pointer;
 }
-
-/* TITLE EDIT PART
-------------------------------------*/
-.title {
-  text-align: center;
-  padding-top: 4%;
-}
-
-.thisIsPart {
-  color: rgb(65, 177, 0);
-  text-shadow: 2px 2px rgb(0, 0, 0);
-}
-
-.titleFSpart {
-  color: rgb(255, 255, 255);
+ .btParagraf {
+  font-size: 26px;
   font-family: "Bangers", cursive;
-  text-shadow: 2px 2px black;
+  color: rgb(119, 158, 122);
 }
-.areaPart {
-  color: rgb(0, 0, 0);
-  text-shadow: 2px 2px rgb(11, 114, 2);
+.buttons{
+  display: block;
+  width: 50%;
+  margin: 0 auto;
 }
-
-/* ----------------------------------*/
-
 .monitor {
-  height: 200px;
-  width: 200px;
+  height: 50px;
+  width: 50%;
   display: flex;
   align-items: top;
-  justify-content: flex-start;
-  border-radius: 10%;
-  border-width: 8px;
-  border-style: solid;
-  border-color: rgb(55, 56, 54);
+  justify-content: center;
   font-family: "Righteous", cursive;
   text-align: left;
   font-size: 14px;
-  background-image: url(../assets/monitor.png);
-  margin-top: 10%;
   margin:0 auto;
-  margin-top: 10%;
 }
 .textType {
   padding: 10px;
-  color: rgb(157, 255, 0);
+  color: rgb(119, 158, 122);
 }
-.getInBtn {
-  align-content: center;
-  width: 80%;
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  height: 20%;
-  font-size: 20px;
-  margin: 5px auto;
-  background-color: rgba(224, 20, 20, 0);
-  border-radius: 10px;
-  border-width: 8px;
-  border-style: solid;
-  border-color: rgb(255, 0, 0);
-  max-height: 200px;
-  max-width: 300px;
+.wrap{
+  position: fixed;
+  bottom: 10vh;
+  width: 50% !important;
+  margin: 0 auto;
 }
-.btParagraf {
-  float: left;
-  font-size: 35px;
-  font-family: "Bangers", cursive;
-  color: rgb(255, 0, 0);
-  padding-top: 2%;
+.images{
+  display: block;
+  width: 100%;
 }
-.imageShip {
-  float: right;
-  margin-right: 5%;
-  margin-left: 20px;
+.images img{
+  display: block;
+  width: 80vw;
+  height: 50vh;
+  margin: 0 auto;
 }
-
-
 </style>
--- src/components/popup.vue
+++ src/components/popup.vue
@@ -1,14 +1,23 @@
 <template>
     <div class="popwrap">
-        <div>
-            <div class="popupwrap">
-                <p id="poptext"></p>
-            </div>
-            <div class="input">
-                    <input type="text" v-model="name" placeholder="Anonymous" @keyup.enter="submitName">
-                    <button @click="submitName">Oki</button>
-            </div>
+      <header>
+        <div class="imageShip">
+          <img src="../assets/mainlogo.jpg"/>
         </div>
+        <h1 class="title"><span class="thisIsPart">this is</span>
+          <br><span class="titleFSpart">F - S </span>
+          <span class="areaPart"><br> App</span>
+        </h1>
+      </header>
+      <div>
+          <div class="popupwrap">
+              <p id="poptext"></p>
+          </div>
+          <div class="input">
+                  <input type="text" v-model="name" placeholder="Anonymous" @keyup.enter="submitName">
+                  <button @click="submitName">Oki</button>
+          </div>
+      </div>
     </div>
 </template>
 
@@ -76,64 +85,114 @@ export default {
 </script>
 
 <style>
-#poptext{
-    color:rgb(255, 0, 0);
-    font-weight: bolder;
-    margin: 2px 2px;
+
+.areaPart,
+.title{
+  color: rgb(119, 158, 122);
+  text-shadow: 2px 2px #fff;
 }
-.popwrap{
+.titleFSpart{
+  color: #fff;
+  text-shadow: 2px 2px #fff;
+}
+.tatko{
+    background-color: #000000;
     display: flex;
-    justify-content: center;
     align-items: center;
-    height: 100vh;
-    width: 100vw;
-}
-.popupwrap{
-    background-image: url(../assets/monitor.png);
-    width: 80%;
-    height: 61px;
-    margin:  auto;
-    display: flex;
     justify-content: center;
+    align-content: center;
+    margin: 0 auto;
+    width: 100%;
+    overflow: hidden;
+}
+.wrap{
+    width: 100%;
+}
+
+
+header {
+  height: 200px; /* Deo za promenu  */
+  font-size: 20px;
+  font-family: "Righteous", cursive, "Ultra", serif;
+  width: 100%;
+}
+
+/* TITLE EDIT PART
+------------------------------------*/
+.title {
+  text-align: center;
+}
+
+.thisIsPart {
+  color: rgb(119, 158, 122);
+  text-shadow: 2px 2px cyan;
+}
+
+.titleFSpart {
+  color: rgb(255, 255, 255);
+  letter-spacing: 3px;
+  font-family: "Bangers", cursive;
+  text-shadow: 2px 2px black;
+}
+.areaPart {
+  color: rgb(119, 158, 122);
+  text-shadow: 2px 2px cyan;
+}
+
+/* ----------------------------------*/
+
+.popupwrap{
+    width: 50%;
+    height: 61px;
+    margin: 0 auto;
+    display: block;
+    clear: both;
+    margin-top: 15%;
 }
 .input{
+    width: 30%;
     margin: 0 auto;
     display: flex;
     align-items: center;
     justify-content: center;
-    flex-direction: column;
+    flex-direction: row;
 }
 .input input{
-    color: red;
-    background-color: rgba(112, 112, 112, 0.267);
-    width: 199px;
-    height: 35px;
+    width: 78%;
     text-align: center;
     border-width: 0px;
-
+    height: 30px;
+    border-radius: 12px;
+    background: #fafafa;
+    color: cyan;
+    font-size: 16px;
+    padding-left: 20px;
 }
 .input input::placeholder{
-    color: red;
+    color: cyan;
     font-size: 15px;
     font-weight:bolder;
 }
 .input input:focus{
-    border-color: red;
+    border-color: gray;
 }
 .input input:focus::placeholder{
        color:transparent;
        font-size: 20px;
   }
 .input button{
-    margin-top: 2px;
-    margin-left: 10px;
-    border-radius: 50%;
-    font-size: 15px;
-    width: 50px;
-    height: 50px;
-    font-weight:bolder;
-    background-color: rgba(112, 112, 112, 0.267);
-    color: rgb(141, 0, 0);
-    border-width: 0px;
+    margin: 2px;
+    color: cyan;
+    border: none;
+    outline: none;
+    width: 20%;
+    height: 40px;
+    background-color: rgba(112, 112, 112, 0.867);
+    font-size: 14px;
+    font-weight: bold;
+    border-radius: 20px;
+    text-decoration: none;
+    cursor: pointer;
 }
+
 </style>
