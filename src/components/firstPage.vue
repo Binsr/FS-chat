7f2cd72f303323aff599070b44b58e550669a63b
Binsr <urosjovanovic.sna@gmail.com>
2019-10-22 17:05:40 +0200

    Functions storage

--- src/components/firstPage.vue
+++ src/components/firstPage.vue
@@ -53,50 +53,7 @@ export default {
     },
     methods: {
       ...mapActions(['addUsername','client']),
-        // typing() {
-        //   var i = 0;
-        //   var j = 0;
-        //   let text = "";
-        //   if(this.user.ip != "NOT_CONNECTED")
-        //     text = "Videcemo";
-        //   else{
-        //     text = "Na zalost ne postoji cet soba za mesto gde se nalazite trenutno";
-        //   }
-        //   const el = document.getElementById("text");
-        //   if(this.user.ip == "NOT_CONNECTED")
-        //     document.getElementById("text").style.color = "red";
-        //   if(el.textContent.length != 0){
-        //     el.textContent = "";
-        //   }
-        //   this.interval = setInterval(function () {
-        //     if (i < text.length) {
-        //       el.textContent += text[i];
 
-        //       if (i == text.length) {
-        //         i = 0;
-        //         el.textContent = "";
-        //       }
-        //     }
-        //     if (i > text.length) {
-        //       if (i == (text.length + 30)) {
-        //         /* when text end how long till typing again */
-        //         i = -1;
-        //         el.textContent = "";
-        //       }
-        //     }
-        //     if (j % 20 == 0) {
-        //       document.getElementById("clickme").style.color = "rgb(255, 255, 255)";
-        //     }
-        //     if (j % 40 == 0) {
-        //       document.getElementById("clickme").style.color = "rgb(0, 0, 0)";
-        //     }
-        //     if (j == 40) {
-        //       j = 0;
-        //     }
-        //     j++;
-        //     i++;
-        //   }, 90);
-        // },
         showCaffes(){
           if(this.caffeApi){
             api.getCaffeNames().then(Response => {
